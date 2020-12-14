58b6e69aee83a08a9e9391a6f01574da807d6ff4
Marcin Sztylko <marcin.sztylko@gmail.com>
2020-12-14 15:20:00 +0100

    add custom title

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', current_title='Custom title')
 
 if __name__ == '__main__':
     app.run(debug=True)
