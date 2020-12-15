fa70948de1dc5d1af02b1d6b047686526c122eb7
Marcin Sztylko <marcin.sztylko@gmail.com>
2020-12-15 13:20:00 +0100

    add basic forms

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask
 
 
 app = Flask(__name__)
-
+app.config['SECRET_KEY'] = 'dev'
 
 from routes import *
 
--- routes.py
+++ routes.py
@@ -1,11 +1,14 @@
 from app import app
 from flask import render_template
 
+import forms
+
 @app.route('/')
 @app.route('/index')
 def index():
-    return render template('index.html')
+    return render_template('index.html')
 
-@app.route
+@app.route('/about')
 def about():
-    return render_template('about.html')
+    form = forms.AddTaskForm()
+    return render_template('about.html', form=form)
