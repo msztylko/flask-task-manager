ea52a133da7887333fc046437839938ad53e1204
Marcin Sztylko <marcin.sztylko@gmail.com>
2020-12-15 10:20:00 +0100

    add about page function

--- app.py
+++ app.py
@@ -4,8 +4,14 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
+@app.route('/index')
 def index():
     return render_template('index.html', current_title='Custom title')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
