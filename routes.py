c6ee595c12d51ccc4c34efc62825bed1c06f075a
Marcin Sztylko <marcin.sztylko@gmail.com>
2020-12-16 13:40:00 +0100

    update about.html

--- routes.py
+++ routes.py
@@ -13,4 +13,5 @@ def about():
     form = forms.AddTaskForm()
     if form.validate_on_submit():
         print('Submitted title', form.title.data)
+        return render_template('about.html', form=form, title=form.title.data)
     return render_template('about.html', form=form)
