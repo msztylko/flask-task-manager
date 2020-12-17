be906a902c7fb42fc0fc56929dd340e70bcff6a8
Marcin Sztylko <marcin.sztylko@gmail.com>
2020-12-17 12:18:00 +0100

    add tasks functionality

--- routes.py
+++ routes.py
@@ -9,14 +9,15 @@ import forms
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html')
+    tasks = Task.query.all()
+    return render_template('index.html', tasks=tasks)
 
-@app.route('/about', methods=['GET', 'POST'])
-def about():
+@app.route('/add', methods=['GET', 'POST'])
+def add():
     form = forms.AddTaskForm()
     if form.validate_on_submit():
         t = Task(title=form.title.data, date=datetime.utcnow())
         db.session.add(t)
         db.session.commit()
         return redirect(url_for('index'))
-    return render_template('about.html', form=form)
+    return render_template('add.html', form=form)
