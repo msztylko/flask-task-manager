9e91f65e851c77a3532ba355d161ceaa170dfc1b
Marcin Sztylko <marcin.sztylko@gmail.com>
2020-12-20 10:28:00 +0100

    fix delete function

--- routes.py
+++ routes.py
@@ -1,10 +1,9 @@
 from app import app, db
 from flask import render_template, redirect, url_for, flash, get_flashed_messages
-from models import Task
 from datetime import datetime
 
 import forms
-
+from models import Task
 
 @app.route('/')
 @app.route('/index')
@@ -12,9 +11,10 @@ def index():
     tasks = Task.query.all()
     return render_template('index.html', tasks=tasks)
 
-@app.route('/add', methods=['GET', 'POST'])
+
+@app.route('/about', methods=['GET', 'POST'])
 def add():
-    form = forms.AddTaskForm()
+    form  = forms.AddTaskForm()
     if form.validate_on_submit():
         t = Task(title=form.title.data, date=datetime.utcnow())
         db.session.add(t)
@@ -22,7 +22,6 @@ def add():
         flash('Task added to the database')
         return redirect(url_for('index'))
     return render_template('add.html', form=form)
-
 
 @app.route('/edit/<int:task_id>', methods=['GET', 'POST'])
 def edit(task_id):
@@ -34,7 +33,7 @@ def edit(task_id):
             task.title = form.title.data
             task.date = datetime.utcnow()
             db.session.commit()
-            flash('Task has been updated')
+            flash('Task has been updated.')
             return redirect(url_for('index'))
 
         form.title.data = task.title
@@ -51,9 +50,11 @@ def delete(task_id):
     if task:
         if form.validate_on_submit():
             db.session.delete(task)
-            flash('Task has been deleted')
+            db.session.commit()
+            flash('Task has been deleted.')
             return redirect(url_for('index'))
+            
         return render_template('delete.html', form=form, task_id=task_id, title=task.title)
     else:
-        flash('Task not found')
+        flash('Task not found.')
     return redirect(url_for('index'))
